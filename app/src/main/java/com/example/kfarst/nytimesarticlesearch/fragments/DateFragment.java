2bdd1d29a85b504c36ecf2a84abac8a28c77e717
Kevin Farst <kevin.farst@zozi.com>
2016-07-22 20:00:28 -0700

    Add start date picker

--- app/src/main/java/com/example/kfarst/nytimesarticlesearch/fragments/DateFragment.java
+++ app/src/main/java/com/example/kfarst/nytimesarticlesearch/fragments/DateFragment.java
@@ -8,9 +8,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.appeaser.sublimepickerlibrary.SublimePicker;
+import com.appeaser.sublimepickerlibrary.datepicker.SelectedDate;
+import com.appeaser.sublimepickerlibrary.helpers.SublimeListenerAdapter;
+import com.appeaser.sublimepickerlibrary.recurrencepicker.SublimeRecurrencePicker;
 import com.example.kfarst.nytimesarticlesearch.R;
 
 public class DateFragment extends Fragment {
+    SublimePicker mSublimePicker;
+
     public DateFragment() {
         // Required empty public constructor
     }
@@ -30,7 +36,25 @@ public class DateFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_date, container, false);
+
+        SublimeListenerAdapter mListener = new SublimeListenerAdapter() {
+            @Override
+            public void onDateTimeRecurrenceSet(SublimePicker sublimeMaterialPicker, SelectedDate selectedDate, int hourOfDay, int minute, SublimeRecurrencePicker.RecurrenceOption recurrenceOption, String recurrenceRule) {
+
+            }
+
+            @Override
+            public void onCancelled() {
+                // Handle click on `Cancel` button
+            }
+        };
+
+        mSublimePicker = (SublimePicker) view.findViewById(R.id.sublime_picker);
+        // Passing `null` to apply default options
+        mSublimePicker.initializePicker(null, mListener);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_date, container, false);
+        return view;
     }
 }
